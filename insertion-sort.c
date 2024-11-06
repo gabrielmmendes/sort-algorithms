4ab399c2ba3f7c7ccdc1d17e8f2bf6c5219d4690
gabrielmmendes <61764302+gabrielmmendes@users.noreply.github.com>
2024-11-06 08:40:20 -0300

    Create insertion-sort.c

---
+++ insertion-sort.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+// INSERTION SORT
+// Worst case: O(n²)
+// Medium case: O(n²)
+// Best case: O(n) if already sorted 
+
+void ordena(int arr[], int n, int isAsc) {
+    if (isAsc) {
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        } 
+    } else {
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] < key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        } 
+    }
+    
+}
+
+int main() {
+    
+    int n = 10;
+    int arr[] = {4, 1, 3, 6, 7, 8, 2, 9, 0, 5};
+
+    
+    ordena(arr, n, 1);
+    
+    printf("Asc:\n");
+    for (int i = 0; i < n; i++) {
+        printf(" %d", arr[i]);
+    }
+    
+    ordena(arr, n, 0);
+    
+    printf("\nDesc:\n");
+    for (int i = 0; i < n; i++) {
+        printf(" %d", arr[i]);
+    }
+    
+    return 0;
+}
